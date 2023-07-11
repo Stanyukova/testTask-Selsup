e25a92fe698712cba1c4c07a5c68b5bc3033fb46
Mari <mstanyukova@gmail.com>
2023-07-11 17:16:56 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface Color {
+}
+
+interface Param {
+  id: number;
+  name: string;
+  type: 'string';
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  colors: Color[];
+}
+interface Props {
+  params: Param[];
+  model: Model;
+  onParamChange: (paramId: number, value: string) => void;
+}
+interface AppState {
+  params: Param[];
+  model: Model;
+}
+class ParamEditor extends React.Component<Props> {
+  handleParamChange = (paramId: number, e: React.ChangeEvent<HTMLInputElement>) => {
+    const { onParamChange } = this.props;
+    const value = e.target.value;
+    onParamChange(paramId, value);
+  };
+
+  render() {
+    const { params, model } = this.props;
+    const { paramValues } = model;
+
+    return (
+      <div>
+        {params.map((param) => {
+          const paramValue = paramValues.find((pv) => pv.paramId === param.id);
+          const value = paramValue ? paramValue.value : '';
+
+          return (
+            <div key={param.id}>
+              <label htmlFor={`param_${param.id}`}>{param.name}</label>
+              <input
+                type="text"
+                id={`param_${param.id}`}
+                value={value}
+                onChange={(e) => this.handleParamChange(param.id, e)}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      params: [
+        { id: 1, name: 'Назначение', type: 'string' },
+        { id: 2, name: 'Длина', type: 'string' },
+      ],
+      model: {
+        paramValues: [
+          { paramId: 1, value: 'повседневное' },
+          { paramId: 2, value: 'макси' },
+        ],
+        colors: [],
+      },
+    };
+  }
+
+  handleParamChange = (paramId: number, value: string) => {
+    const { model } = this.state;
+    const { paramValues } = model;
+
+    const updatedParamValues = paramValues.map((pv) =>
+      pv.paramId === paramId ? { ...pv, value } : pv
+    );
+console.log(updatedParamValues)
+    this.setState({
+      model: {
+        ...model,
+        paramValues: updatedParamValues,
+      },
+    });
+  };
+
+  render() {
+    const { params, model } = this.state;
+
+    return (
+      <div>
+        <ParamEditor
+          params={params}
+          model={model}
+          onParamChange={this.handleParamChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
